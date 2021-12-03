dee0325755123082dce0c3f83c2e930929fa71ab
Salik47 <msalik32@gmail.com>
2021-12-04 03:13:18 +0530

    Challenge for Day 23 completed

--- Day23.js
+++ Day23.js
@@ -20,7 +20,7 @@ Output: The lowest index N where the side to the left of N is equal to the side 
 
 Note: If you are given an array with multiple answers, return the lowest correct index.
 
-Test Cases
+Test Cases:
 findEvenIndex([1,2,3,4,3,2,1] should return 3
 findEvenIndex([1,100,50,-51,1,1]) should return 1
 findEvenIndex([1,2,3,4,5,6]) should return -1
